e867fbe575b5698424578c72793bea45a8fcfe16
Luis Cifre <Luis Cifre>
2017-01-19 03:30:51 +0100

    Acabado el Kata de Yatzy

--- Yatzy_Kata_Refactoring/Yatzy.java
+++ Yatzy_Kata_Refactoring/Yatzy.java
@@ -1,5 +1,7 @@
 package org.foogbarspam.Yatzy.tirada;
 
+import java.util.Arrays;
+
 public class Yatzy {
 	protected int[] dice;
 	
@@ -91,7 +93,109 @@ public class Yatzy {
 		return score;
 	}
 	
+	public static int ScorePair(int...dice){
+		int score = 0;
+		Arrays.sort(dice);
+		for (int i = 4; i > 0; i--){
+			if (dice[i] == dice[i-1]){
+				return score = dice[i]*2;
+			}
+		}
+		return score;
+	}
 	
+	public static int ScoreTwoPairs(int...dice){
+		int score = 0;
+		int pairsFound = 0;
+		Arrays.sort(dice);
+		for (int i = 4; i > 0; i--){
+			if(dice[i] == dice[i-1]){
+				score += dice[i] * 2;
+				pairsFound += 1;
+				i -= 1;
+			}
+			if(pairsFound == 2){
+				return score;
+			}
+		}
+		return 0;
+	}
+	
+	
+	public static int ThreeOfAKind(int...dice){
+		int score = 0;
+		Arrays.sort(dice);
+		for (int i = 4; i>1; i--){
+			if ((dice[i] == dice[i-2])){
+				return score = dice[i] * 3;
+			}
+		}		
+		return score;
+	}
+	
+	public static int FourOfAKind(int...dice){
+		int score = 0;
+		Arrays.sort(dice);
+		for (int i = 4; i>2; i--){
+			if((dice[i] == dice[i-3])){
+				return score = dice [i] * 4;
+			}
+		}
+		return score;
+	}
+	
+	public static int SmallStraight(int...dice){
+		int score = 15;
+		Arrays.sort(dice);
+		if (dice[0] != 1){
+			return 0;
+		}
+		for (int i = 0; i < dice.length - 1; i++){
+			if (dice[i+1] != (dice[i] + 1)){
+				return 0;
+			}
+		}
+		return score;
+	}
+	
+	public static int LargeStraight(int...dice){
+		int score = 20;
+		Arrays.sort(dice);
+		if (dice[0] == 1){
+			return 0;
+		}
+		for (int i = 0; i < dice.length - 1; i++){
+			if (dice[i+1] != (dice[i] + 1)){
+				return 0;
+			}
+		}
+		return score;
+	}
+	
+	//Creamos un método para buscar un par en las 2 primeras posiciones
+	//o en las 2 últimas, siempre que haya 3 números iguales en las otras
+	//posiciones.
+	
+	public static int OnePair(int...dice){
+		int score = 0;
+		Arrays.sort(dice);
+		if (dice[4] == dice[2] && dice[1] == dice[0]){
+			return score = dice[0] *2;
+		}else if(dice[2] == dice[0] && dice[4] == dice[3]){
+			return score = dice[4] * 2;
+		}else{
+			return score;
+		}
+	}
+	
+	
+	public static int FullHouse(int...dice){
+		int score = 0;
+		if (Yatzy.ThreeOfAKind(dice) != 0 && Yatzy.OnePair(dice) != 0){
+			score = Yatzy.ThreeOfAKind(dice) + Yatzy.OnePair(dice);
+		}
+		return score;
+	}
 }
 
 
